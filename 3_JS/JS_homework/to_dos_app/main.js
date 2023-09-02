bd366f4ac1e213dd9468cd23ddfe00e899df2034
Alexandru BOSCU <boscu20@gmail.com>
2023-09-02 21:14:39 +0300

    wrote documentation for todos app homework

--- 3_JS/JS_homework/to_dos_app/main.js
+++ 3_JS/JS_homework/to_dos_app/main.js
@@ -1,3 +1,6 @@
+// Documentation wroted by mintlify.document
+// Mintlify Doc Writer for Python, JavaScript, TypeScript, C++, PHP, Java, C#, Ruby & more
+
 import userTasks from './script/state.js';
 
 const taskTitle = document.getElementById('todo__title');
@@ -11,18 +14,38 @@ const clearSearchBtn = document.getElementById('todo__clear__btn');
 const todoListEl = document.getElementById('to__do__tasks__list');
 const doneListEl = document.getElementById('done__items__list');
 
+/**
+ * The function handleLocalStorage() saves the userTasks array to the localStorage as a JSON string.
+ */
 function handleLocalStorage() {
   localStorage.setItem('todos', JSON.stringify(userTasks));
 }
 
+/**
+ * The function generates a random ID within a specified range.
+ * @param [min=10000000] - The minimum value that the random ID can be. By default, it is set to
+ * 10,000,000.
+ * @param [max=20000000] - The `max` parameter is the maximum value that the generated random ID can
+ * have. By default, it is set to 20,000,000.
+ * @returns a randomly generated ID within the specified range.
+ */
 function generateRandomId(min = 10000000, max = 20000000) {
   const id = Math.floor(Math.random() * (max - min) + min);
   return id;
 }
 
+/**
+ * The function `insertTaskInDOM` creates and inserts a task element into the DOM, including a
+ * checkbox, task title, description, and delete button.
+ * @param task - The `task` parameter is an object that represents a task. It should have the following
+ * properties:
+ * @param parent - The `parent` parameter is the DOM element where the task will be inserted. It should
+ * be a reference to the parent element, such as `document.getElementById('parentElementId')`.
+ */
 function insertTaskInDOM(task, parent) {
-  // task param is an object
-  // create list element
+  /* The code is creating a new list element (`<li>`) and assigning it a class of `'to__do__task'` and an
+id equal to the `id` property of the `task` object. This list element will be used to represent a
+task in the DOM. */
   const listEl = document.createElement('li');
   listEl.classList.add('to__do__task');
   listEl.id = task.id;
@@ -41,7 +64,10 @@ function insertTaskInDOM(task, parent) {
   }
   doneBtnEL.append(inputEl);
 
-  // add event listener on checkbox to mark when a task it's done
+  // add event listener on checkbox to mark / unmark the task as beeing done or not
+  /* The `inputEl.addEventListener('change', (e) => { ... })` code adds an event listener to the
+  checkbox element (`inputEl`). When the checkbox is changed (checked or unchecked), the callback
+  function is executed. */
   inputEl.addEventListener('change', (e) => {
     if (inputEl.checked) {
       const aux = userTasks.todo.filter((object) => object.id == listEl.id);
@@ -68,11 +94,11 @@ function insertTaskInDOM(task, parent) {
   const taskEl = document.createElement('div');
   taskEl.classList.add('task__content');
 
-  // task title
+  // create a h2 element for task title
   const titleEl = document.createElement('h2');
   titleEl.textContent = task.title;
 
-  //tsk description
+  // create a p element for task description
   const descriptionEl = document.createElement('p');
   descriptionEl.textContent = task.description;
   taskEl.append(titleEl);
@@ -102,29 +128,43 @@ function insertTaskInDOM(task, parent) {
   parent.append(listEl);
 }
 
+/**
+ * The function `insertTaskInDOMOnLoad` retrieves tasks from local storage, clears the task lists, and
+ * inserts the tasks into the appropriate DOM elements.
+ */
 function insertTaskInDOMOnLoad() {
-  const obj = JSON.parse(localStorage.getItem('todos'));
-
   todoListEl.innerHTML = '';
   doneListEl.innerHTML = '';
-  userTasks.done.length = 0;
-  userTasks.todo.length = 0;
+  // userTasks.done.length = 0;
+  // userTasks.todo.length = 0;
 
-  obj.todo.forEach((object) => {
+  userTasks.todo.forEach((object) => {
     insertTaskInDOM(object, todoListEl);
-    userTasks.todo.push(object);
+    // userTasks.todo.push(object);
   });
 
-  obj.done.forEach((object) => {
+  userTasks.done.forEach((object) => {
     insertTaskInDOM(object, doneListEl);
-    userTasks.done.push(object);
+    // userTasks.done.push(object);
   });
 }
 
+/* The `window.addEventListener('load', () => { insertTaskInDOMOnLoad(); });` code is adding an event
+listener to the `window` object for the `load` event. When the `load` event is triggered, it calls
+the `insertTaskInDOMOnLoad()` function. This function is responsible for retrieving tasks from local
+storage, clearing the task lists, and inserting the tasks into the appropriate DOM elements. By
+adding this event listener, the `insertTaskInDOMOnLoad()` function will be executed once all the
+resources on the page have finished loading. */
 window.addEventListener('load', () => {
   insertTaskInDOMOnLoad();
 });
 
+/**
+ * The `addTask` function adds a new task with a title and description to the user's todo list.
+ * @param title - The title parameter is a string that represents the title of the task.
+ * @param description - The `description` parameter is a string that represents the description of the
+ * task. It provides additional details or information about the task.
+ */
 function addTask(title, description) {
   const auxObj = {};
   auxObj.id = generateRandomId();
@@ -138,6 +178,14 @@ function addTask(title, description) {
   handleLocalStorage();
 }
 
+/**
+ * The function removes a task from either the "todo" or "done" array in the "userTasks" object based
+ * on the provided id and the parent element.
+ * @param id - The id parameter represents the unique identifier of the task that needs to be removed.
+ * @param elemParent - The `elemParent` parameter is a string that represents the parent element of the
+ * task that needs to be removed. It can have two possible values: "to__do__tasks__list" or
+ * "done__tasks__list".
+ */
 function removeTask(id, elemParent) {
   const newTasks =
     elemParent === 'to__do__tasks__list'
@@ -151,17 +199,27 @@ function removeTask(id, elemParent) {
   handleLocalStorage();
 }
 
+/* The code `addTaskBtn.addEventListener('click', () => { addTask(taskTitle.value,
+taskDescription.value); taskTitle.value = ''; taskDescription.value = ''; });` is adding an event
+listener to the `addTaskBtn` element. When the button is clicked, the callback function is executed. */
 addTaskBtn.addEventListener('click', () => {
   addTask(taskTitle.value, taskDescription.value);
   taskTitle.value = '';
   taskDescription.value = '';
 });
 
+/* The `searchTaskBtn.addEventListener('click', () => { ... })` code adds an event listener to the
+`searchTaskBtn` element. When the button is clicked, the callback function is executed. */
 searchTaskBtn.addEventListener('click', () => {
-  todoListEl.innerHTML = '';
-  doneListEl.innerHTML = '';
+  /* This code is performing a search functionality based on the user's input query. */
   const query = searchQuery.value.toLowerCase();
 
+  todoListEl.innerHTML = '';
+  doneListEl.innerHTML = '';
+  /* The code is creating a new array called `todoTasks` by filtering the `userTasks.todo` array. It
+  filters the array based on whether the `title` or `description` of each task includes the `query`
+  string (after converting both the `title` and `description` to lowercase). The spread operator
+  (`...`) is used to create a new array with the filtered tasks. */
   const todoTasks = [
     ...userTasks.todo.filter(
       (object) =>
@@ -169,7 +227,9 @@ searchTaskBtn.addEventListener('click', () => {
         object.description.toLowerCase().includes(query)
     ),
   ];
-
+  /* The code `todoTasks.forEach((object) => { insertTaskInDOM(object, todoListEl); });` is iterating
+  over each task object in the `todoTasks` array and calling the `insertTaskInDOM` function for each
+  task. */
   todoTasks.forEach((object) => {
     insertTaskInDOM(object, todoListEl);
   });
@@ -181,12 +241,14 @@ searchTaskBtn.addEventListener('click', () => {
         object.description.toLowerCase().includes(query)
     ),
   ];
-
   doneTasks.forEach((object) => {
     insertTaskInDOM(object, doneListEl);
   });
 });
 
+/* The code `clearSearchBtn.addEventListener('click', () => { searchQuery.value = '';
+insertTaskInDOMOnLoad(); });` adds an event listener to the `clearSearchBtn` element. When the
+button is clicked, the callback function is executed. */
 clearSearchBtn.addEventListener('click', () => {
   searchQuery.value = '';
 
--- 3_JS/JS_homework/to_dos_app/script/state.js
+++ 3_JS/JS_homework/to_dos_app/script/state.js
@@ -1,4 +1,10 @@
-const userTasks = {
+/* This code is retrieving the value stored in the 'todos' key of the localStorage object and parsing
+it as a JSON object using `JSON.parse()`. If there is no value stored in the 'todos' key or if the
+value cannot be parsed as a JSON object, it will assign an empty object with properties `todo` and
+`done` set to empty arrays as the default value. Finally, it exports the `userTasks` object as the
+default value for the module. */
+
+const userTasks = JSON.parse(localStorage.getItem('todos')) || {
   todo: [],
   done: [],
 };
